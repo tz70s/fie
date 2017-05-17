04679e9bae91369918b2515745bf312b8a50ff43
tz70s <su3g4284zo6y7@gmail.com>
2017-05-17 13:54:38 +0800

    extends class methods and add automatic container routing
    routeAll()

--- bighost.py
+++ bighost.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
 from subprocess import call
+from subprocess import STDOUT
+import os
 import docker
 
 from mininet.net import Mininet
@@ -20,26 +22,15 @@ Architecture:
 
 """
 
-
-"""
-Class bighost for creating object associate with network namespace and docker containers/bridges informations
-
-pairNetns() : create veth in network namespace
-
-setIPpool() : set the network ip pool for new containers over specific network namespace
-
-createBridge() : create docker network a.k.a linux bridge
-
-patchBridge() : patch the linux bridge with namespace veth
-
-setNATrule() : set NAT rules
-
-
-"""
 class bighost():
-    
-    def __init__(self, pid, name, ip_pool, docker_client):
-        self.pid = pid
+        
+    """
+    Initialize host/namespace informations
+
+    """
+    def __init__(self, host, name, ip_pool, docker_client):
+        self.host = host
+        self.pid = str(host.pid)
         self.name = name
         self.ip_pool = ip_pool
         self.docker_client = docker_client
@@ -47,7 +38,20 @@ class bighost():
         sub_list = ip_pool.split('/')[0].split('.')
         sub_list[-1] = '1'
         self.gw = '.'.join(sub_list)
+    
+        self.container_list = []
 
+    """
+    Inheritent mininet host cmd
+    
+    """
+    def cmd(self, cmdstr):
+        self.host.cmd(cmdstr)
+
+    """
+    create veth in network namespace
+    
+    """
     def pairNetns(self):
         print(self.name + ' : add netns veth pair with docker bridge')
         
@@ -58,15 +62,24 @@ class bighost():
         call(create_link.split(' '))
         call(up_link.split(' '))
         call(set_ns.split(' '))
-    
+        
+        self.cmd( 'ifconfig ' + self.name + '-eth1 ' + self.gw)
+        self.SetNATrules()
 
+    """
+    Set the network ip pool for new containers over specific network namespace
+    
+    """
     def setIPpool(self):
 
         ipam_pool = docker.types.IPAMPool( subnet = self.ip_pool)
         ipam_config = docker.types.IPAMConfig( pool_configs = [ipam_pool] )
 
         return ipam_config
-    
+    """
+    Create docker network a.k.a linux bridge
+
+    """
     def createBridge(self):
         opts = {
             'com.docker.network.bridge.name': 'netns-'+self.name,
@@ -74,22 +87,60 @@ class bighost():
         self.dockerbridge = self.docker_client.networks.create('netns-'+self.name, driver='bridge', ipam=self.setIPpool(), options=opts)
         self.patchBridge()
     
+    """
+    Patch veth with docker network bridges
+
+    """
+    
     def patchBridge(self):
         call(['brctl', 'addif', 'netns-'+self.name, 'docker-'+self.name+'-port'])
         call(['ip', 'link', 'set', 'dev', 'docker-'+self.name+'-port', 'up'])
     
-    def destroy(self):
-        self.dockerbridge.remove()
+    """
+    Remove devices
+    """
 
-    def setNATrule(self):
+    def destroy(self):
+        for container in self.container_list:
+            call(['docker', 'rm', '-f', container], stdout=open(os.devnull, "w"), stderr=STDOUT)
+        self.dockerbridge.remove()
+    
+    """
+    NAT rules setting inner host/namespace
+
+    """
+
+    def NATrules(self):
         postroute = 'iptables -t nat -A POSTROUTING -o ' + self.name + '-eth0 -j MASQUERADE'
         conn = 'iptables -A FORWARD -i ' + self.name + '-eth0 -o ' + self.name + '-eth1 -m state --state RELATED,ESTABLISHED -j ACCEPT'
         accept = 'iptables -A FORWARD -i ' + self.name + '-eth1 -o ' + self.name + '-eth0 -j ACCEPT'
         
         return [postroute, conn, accept]
+    
+    def SetNATrules(self):
+        for rules in self.NATrules():
+            self.cmd(rules)
+    
+    def net(self):
+        self.pairNetns()
+        self.createBridge()
+    
+    """
+    Set static route to a specific container subnet
+    """
+    
+    def route(self, host):
+        dest_ip = host.ip_pool.split('/')[0]
+        dest_gw = host.host.IP(host.name+'-eth0')
+        self.cmd('route add -net ' + dest_ip + ' netmask 255.255.255.0 gw ' + dest_gw + ' dev ' + self.name + '-eth0')
+            
+        print(dest_ip)
+
     def simpleRun(self, image):
         namestr = image.split('/')[-1]
-        call(['docker', 'run', '-itd', '--network=netns-' + self.name , '--name='+self.name+'-'+namestr, image])
+        call(['docker', 'run', '-itd', '--network=netns-' + self.name , '--name='+self.name+'-'+namestr, image], stdout=open(os.devnull, "w"), stderr=STDOUT)
+        self.container_list.append(self.name+'-'+namestr)
+
 
 def setClient():
     client = docker.DockerClient(base_url = 'unix://var/run/docker.sock', version = 'auto')
@@ -118,18 +169,32 @@ Testing for cross-hosts:
 """
 
 
+"""
+Set static route for each host to all container subnets
+
+"""
+def routeAll(*args):
+    for host in args:
+        for another in args:
+            if host == another:
+                continue
+            else:
+                host.route(another)
+            
 def emptyNet():
     
     print("""
-        This work is a demonstration of bridging network namespace in mininet and docker containers
-        
-        Architecture:
-            =======     ========
-            |     |     |Docker|---CONTAINER
-            |netns|=====|      |---CONTAINER
-            |     |     |Bridge|---CONTAINER
-            =======     ========
-        """
+
+This work is a demonstration of bridging network namespace in mininet and docker containers
+    
+Architecture:
+    =======     ========
+    |     |     |Docker|---CONTAINER
+    |netns|=====|      |---CONTAINER
+    |     |     |Bridge|---CONTAINER
+    =======     ========
+    
+    """
     )
     net = Mininet( controller=Controller )
 
@@ -137,7 +202,7 @@ def emptyNet():
 
     h1 = net.addHost('h1', ip='10.0.0.1')
     h2 = net.addHost('h2', ip='10.0.0.2')
-
+    
     s1 = net.addSwitch('s1')
     net.addLink(h1, s1)
     net.addLink(h2, s1)
@@ -148,31 +213,21 @@ def emptyNet():
 
     client = setClient()
 
-    host1 = bighost(str(h1.pid), 'h1', '192.168.52.0/24', client)
+    host1 = bighost(h1, 'h1', '192.168.52.0/24', client)
+    host1.net()
+    
+    host2 = bighost(h2, 'h2', '192.168.53.0/24', client)
+    host2.net()
 
-    host1.pairNetns()
-    host1.createBridge()
-    h1.cmd( 'ifconfig h1-eth1 ' + host1.gw)
-    for rule in host1.setNATrule():
-        h1.cmd(rule)
-
-    host2 = bighost(str(h2.pid), 'h2', '192.168.53.0/24', client)
-    host2.pairNetns()
-    host2.createBridge()
-    h2.cmd('ifconfig h2-eth1 ' + host2.gw)
-    for rule in host2.setNATrule():
-        h2.cmd(rule)
-
-    # container = client.containers.run(image='tz70s/node-server', detach=True, networks=['netns-'+host1.name,], name=host1.name+'app1')
     host1.simpleRun('tz70s/node-server')
     host2.simpleRun('ubuntu')
+    routeAll(host1, host2)
+    
     CLI(net)
-
+    
     net.stop()
 
-    # destroy containers
-    call(['docker', 'rm', '-f', host1.name+'-node-server'])
-    call(['docker', 'rm', '-f', host2.name+'-ubuntu'])
+    # destroy containers and bridges
     host1.destroy()
     host2.destroy()
 
